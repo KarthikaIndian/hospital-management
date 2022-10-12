813245b49a935e861b166f3528ba8e3fb7869fa8
gayathriganesan1996 <gayusharon96@gmail.com>
2022-10-12 16:46:08 +0530

    merge2

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientRegisterComponent } from './patient-register/patient-register.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { DoctorRegisterComponent } from './doctor-register/doctor-register.component';
 
 
 @NgModule({
-  declarations: [   PatientRegisterComponent],
+  declarations: [   PatientRegisterComponent, DoctorRegisterComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
