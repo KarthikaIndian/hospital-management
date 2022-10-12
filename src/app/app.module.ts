498112680e89374a3b3364b264f07811812ef65c
KarthikaIndian <karthika.indian12@gmail.com>
2022-10-12 15:43:15 +0530

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PatientRegisterComponent } from './pages/patient-register/patient-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientRegisterComponent
   ],
   imports: [
     BrowserModule,
