8f1e356c4122b9243dc8fd8e9920b4394ca06510
gayathriganesan1996 <gayusharon96@gmail.com>
2022-10-13 10:07:20 +0530

    merge three

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorRegisterComponent } from './doctor-register/doctor-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'doctor', component: DoctorRegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
