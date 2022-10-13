09ca5f15f0348d05ebca5a9c8dfc6c0b6ce77efc
KarthikaIndian <karthika.indian12@gmail.com>
2022-10-13 15:32:17 +0530

    patient register page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { PatientRegisterComponent } from './patient-register/patient-register.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [   PatientRegisterComponent, DashboardComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatButtonModule
+  
  
   ]
 })
--- src/app/pages/patient-register/patient-register.component.ts
+++ src/app/pages/patient-register/patient-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormArray,FormBuilder,Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-patient-register',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-register.component.scss']
 })
 export class PatientRegisterComponent implements OnInit {
+  patientForm !:FormGroup
+  submitted = false;
+  states: string[] = [
+    'Tamil Nadu',
+    'Kerala',
+    'Andhra Pradesh',
+    'Karnataka',
+    'Goa']
+    contries:string[]=[
+      'India',
+      'Australia'
+    ]
+    cities:string[]=[
+      'Thanjavur',
+      'Madurai',
+      'Chennai'
+    ]
+    status:string[]=[
+      'Married',
+      'Unmarried',
+      'Divorce',
 
-  constructor() { }
+    ]
+  constructor( private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.patientForm = this.formbuilder.group({
+      id: [null],
+      email: [null, [Validators.required, Validators.email]],
+      name:[null,Validators.required],
+      lname:[null,Validators.required],
+      fname:[null,Validators.required],
+      sname:[null,Validators.required],
+      mname:[null,Validators.required],
+      patientNo:[null,Validators.required],
+      phoneNo:[null,Validators.required],
+      altPhoneNo:[null,],
+      title:[null,Validators.required],
+      marital:[null,Validators.required],
+      age:[null,Validators.required],
+      bloodGroup:[null,Validators.required],
+      weight:[null,Validators.required],
+      gender:[null,Validators.required],
+      add1:[null,Validators.required],
+      add2:[null,Validators.required],
+      country:[null,Validators.required],
+      state:[null,Validators.required],
+      city:[null,Validators.required],
+      pinCode:[null,Validators.required],
+      note:[null,Validators.required],
 
+
+    
+    });
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.patientForm.controls;
+  }
+  Submit(){
+    this.submitted=true
+
+  }
 }
